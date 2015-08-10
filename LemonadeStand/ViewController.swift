0792cabe70237e8f97fb0edd323977043379c71d
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-08-10 14:07:14 -0500

    hooked up all the UI elements to the view controller.

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -14,8 +14,17 @@ class ViewController: UIViewController {
     var lemonInventory = 1
     var iceCubeInventory = 1
     
+    //UI Elements
     
-
+    @IBOutlet weak var totalDollars: UILabel!
+    @IBOutlet weak var lemonsInStock: UILabel!
+    @IBOutlet weak var iceCubesInStock: UILabel!
+    @IBOutlet weak var numberOfLemonsForTwoDollars: UIBarButtonItem!
+    @IBOutlet weak var numberOfLemonsPreferred: UIBarButtonItem!
+    @IBOutlet weak var numberOfIceCubesPreferred: UIBarButtonItem!
+    
+    @IBOutlet weak var numberOfIceCubesForOneDollar: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,6 +35,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func addLemonPreferred(sender: UIBarButtonItem) {
+    }
+    @IBAction func subtractLemonPreferred(sender: UIBarButtonItem) {
+    }
+    @IBAction func addLemonForTwoDollars(sender: UIBarButtonItem) {
+    }
+    @IBAction func subtractLemonForTwoDollars(sender: UIBarButtonItem) {
+    }
 
+    @IBAction func addIceCubePreferred(sender: UIBarButtonItem) {
+    }
+    @IBAction func addIceCubeForOneDollar(sender: UIBarButtonItem) {
+    }
+    @IBAction func subtractIceCubeForOneDollar(sender: UIBarButtonItem) {
+    }
+    @IBAction func subtractIceCubePreferred(sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func startDay(sender: AnyObject) {
+    }
 }
 
