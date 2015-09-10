50c338a42ed724dbedabd3d857db48abe2447798
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-09-10 07:20:30 -0500

    added inventory and mix functions to change daily purchase quantities and mix amounts.

--- LemonadeStand/ViewController.swift
+++ LemonadeStand/ViewController.swift
@@ -14,16 +14,26 @@ class ViewController: UIViewController {
     var lemonInventory = 1
     var iceCubeInventory = 1
     
-    //UI Elements
+    var lemonInventorySubTotal = 0
+    var iceCubeInventorySubTotal = 0
     
+    var lemonMixSubTotal = 0
+    var iceMixSubTotal = 0
+    
+    //UI Elements
+   
     @IBOutlet weak var totalDollars: UILabel!
-    @IBOutlet weak var lemonsInStock: UILabel!
-    @IBOutlet weak var iceCubesInStock: UILabel!
-    @IBOutlet weak var numberOfLemonsForTwoDollars: UIBarButtonItem!
-    @IBOutlet weak var numberOfLemonsPreferred: UIBarButtonItem!
+    @IBOutlet weak var totalLemons: UILabel!
+    @IBOutlet weak var totalIceCubes: UILabel!
+    
+    @IBOutlet weak var dailyPurchaseOfIceLabel: UIBarButtonItem!
+    @IBOutlet weak var dailyPurchaseOfLemonsLabel: UIBarButtonItem!
+    
+        @IBOutlet weak var numberOfLemonsPreferred: UIBarButtonItem!
     @IBOutlet weak var numberOfIceCubesPreferred: UIBarButtonItem!
     
-    @IBOutlet weak var numberOfIceCubesForOneDollar: UIBarButtonItem!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,25 +45,74 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func addLemonPreferred(sender: UIBarButtonItem) {
-    }
-    @IBAction func subtractLemonPreferred(sender: UIBarButtonItem) {
-    }
-    @IBAction func addLemonForTwoDollars(sender: UIBarButtonItem) {
+        @IBAction func addLemonForTwoDollars(sender: UIBarButtonItem) {
+        
+        
+//        cashOnHand = cashOnHand - 2
+        lemonInventorySubTotal++
+        dailyPurchaseOfLemonsLabel.title = "\(lemonInventorySubTotal)"
+        
+//        totalDollars.text = "\(cashOnHand)"
+//        totalLemons.text = "\(lemonInventory)"
+//        println("\(cashOnHand) \(lemonInventory)")
+        
     }
     @IBAction func subtractLemonForTwoDollars(sender: UIBarButtonItem) {
+//        cashOnHand = cashOnHand + 2
+        lemonInventorySubTotal--
+        dailyPurchaseOfLemonsLabel.title = "\(lemonInventorySubTotal)"
+
+//        totalDollars.text = "\(cashOnHand)"
+//        totalLemons.text = "\(lemonInventory)"
+//        println("\(cashOnHand) \(lemonInventory)")
     }
 
-    @IBAction func addIceCubePreferred(sender: UIBarButtonItem) {
-    }
+    
     @IBAction func addIceCubeForOneDollar(sender: UIBarButtonItem) {
+//        cashOnHand = cashOnHand - 1
+        iceCubeInventorySubTotal++
+        dailyPurchaseOfIceLabel.title = "\(iceCubeInventorySubTotal)"
+
+    
+//        totalDollars.text = "\(cashOnHand)"
+//        totalIceCubes.text = "\(lemonInventory)"
+//        println("\(cashOnHand) \(iceCubeInventory)")
     }
     @IBAction func subtractIceCubeForOneDollar(sender: UIBarButtonItem) {
-    }
-    @IBAction func subtractIceCubePreferred(sender: UIBarButtonItem) {
+//        cashOnHand = cashOnHand - 1
+        iceCubeInventorySubTotal--
+        dailyPurchaseOfIceLabel.title = "\(iceCubeInventorySubTotal)"
+        
+//        totalDollars.text = "\(cashOnHand)"
+//        totalIceCubes.text = "\(lemonInventory)"
+//        println("\(cashOnHand) \(iceCubeInventory)")
     }
     
+    @IBAction func addLemonPreferred(sender: UIBarButtonItem) {
+        lemonMixSubTotal++
+        numberOfLemonsPreferred.title = "\(lemonMixSubTotal)"
+    }
+    @IBAction func subtractLemonPreferred(sender: UIBarButtonItem) {
+        lemonMixSubTotal--
+        numberOfLemonsPreferred.title = "\(lemonMixSubTotal)"
+    }
+    
+    @IBAction func addIceCubePreferred(sender: UIBarButtonItem) {
+        iceMixSubTotal++
+        numberOfIceCubesPreferred.title = "\(iceMixSubTotal)"
+    }
+    
+    @IBAction func subtractIceCubePreferred(sender: UIBarButtonItem) {
+        iceMixSubTotal--
+        numberOfIceCubesPreferred.title = "\(iceMixSubTotal)"
+    }
+
+    
     @IBAction func startDay(sender: AnyObject) {
+        
+        lemonInventory = lemonInventory + lemonInventorySubTotal
+        iceCubeInventory = iceCubeInventory + iceCubeInventorySubTotal
+        println("\(lemonInventory)", "\(iceCubeInventory)")
     }
 }
 
