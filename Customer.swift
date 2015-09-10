30e8bb52e3c1db0b22c793e647313d4caaa10827
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-09-10 08:20:07 -0500

    added Customers Class and lemonade Ratio

---
+++ Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  LemonadeStand
+//
+//  Created by Hurewitz, Matthew on 9/10/15.
+//  Copyright (c) 2015 Hurewitz, Matthew. All rights reserved.
+//
+
+import Foundation
+
+class Customer {
+    var lemonadePreferenceRatio = Int(arc4random_uniform(UInt32(11))) / 10
+}
